9ca4b163e452cb836130fe32c75b6444e37b0b49
ThaiyalnathB <thaiyalnath2005@gmail.com>
2026-01-28 23:02:17 +0530

    CRUD operations Added for carousel images

--- Eshop/mainapp/urls.py
+++ Eshop/mainapp/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 
 from . import views
 
+from .views import (
+    CarouselImageCreateView, CarouselImageListView, CarouselImagesUpdateView, CarouselImageDeleteView
+)
+
 urlpatterns = [
     path('', views.homeView, name='home_page'),
     path('about/', views.aboutView, name='about_page'),
-    path('contact/', views.contactView, name='contact_page')
+    path('contact/', views.contactView, name='contact_page'),
+
+    path('carousels/', CarouselImageListView.as_view(), name='carousel_list'),
+    path('carousels/add/', CarouselImageCreateView.as_view(), name='carousel_add'),
+    path('carousels/<int:pk>/edit/', CarouselImagesUpdateView.as_view(), name='carousel_edit'),
+    path('carousels/<int:pk>/delete/', CarouselImageDeleteView.as_view(), name='carousel_delete')
 ]
 
