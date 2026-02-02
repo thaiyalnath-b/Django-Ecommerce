4f75031bef3495125d25e0309adf340569f605e3
ThaiyalnathB <thaiyalnath2005@gmail.com>
2026-02-02 21:52:15 +0530

    Add to cart Added

--- Eshop/Eshop/urls.py
+++ Eshop/Eshop/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     
     path('accounts/', include('authentication.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('cart/', include('cart.urls'))
 ]
 
 if settings.DEBUG == True:
